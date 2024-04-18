5d9a47b1d64913671fdb468ee54da7763b744a81
timothijoe <zt1301112@gmail.com>
2024-04-19 01:13:25 +0800

    add hybrid astar addition draw vehicle

--- PathPlanning/src/GlobalPlanner/HybridAstar/hybrid_astar.cpp
+++ PathPlanning/src/GlobalPlanner/HybridAstar/hybrid_astar.cpp
@@ -35,6 +35,97 @@ constexpr double STEER_CHANGE_COST = 5.0;     // steer angle change penalty cost
 constexpr double STEER_ANGLE_COST = 1.0;      // steer angle penalty cost
 constexpr double H_COST = 15.0;               // Heuristic cost penalty cost
 
+static void plot(VectorXd vec_x, VectorXd vec_y, std::string style) {
+    std::vector<double> x;
+    std::vector<double> y;
+    double* x_pointer = vec_x.data();
+    double* y_pointer = vec_y.data();
+    x.assign(x_pointer, x_pointer + vec_x.rows());
+    y.assign(y_pointer, y_pointer + vec_y.rows());
+
+    plt::plot(x, y, style);
+}
+void draw_arrow(double x, double y, double theta, double L, std::string color) {
+    double angle = M_PI / 6;
+    double d = 0.3 * L;
+
+    double x_start = x;
+    double y_start = y;
+    double x_end = x + L * cos(theta);
+    double y_end = y + L * sin(theta);
+
+    double theta_hat_L = theta + M_PI - angle;
+    double theta_hat_R = theta + M_PI + angle;
+
+    double x_hat_start = x_end;
+    double x_hat_end_L = x_hat_start + d * cos(theta_hat_L);
+    double x_hat_end_R = x_hat_start + d * cos(theta_hat_R);
+
+    double y_hat_start = y_end;
+    double y_hat_end_L = y_hat_start + d * sin(theta_hat_L);
+    double y_hat_end_R = y_hat_start + d * sin(theta_hat_R);
+
+    plt::plot({x_start, x_end}, {y_start, y_end}, color);
+    plt::plot({x_hat_start, x_hat_end_L}, {y_hat_start, y_hat_end_L}, color);
+    plt::plot({x_hat_start, x_hat_end_R}, {y_hat_start, y_hat_end_R}, color);
+}
+
+void draw_vehicle1(Vector3d state, double steer, utils::VehicleConfig c, std::string color= "-k", bool show_wheel = true,
+                  bool show_arrow= true) {
+    Matrix<double, 2, 5> vehicle;
+    Matrix<double, 2, 5> wheel;
+    vehicle << -c.RB, -c.RB, c.RF, c.RF, -c.RB, c.W / 2, -c.W / 2, -c.W / 2, c.W / 2, c.W / 2;
+    wheel << -c.TR, -c.TR, c.TR, c.TR, -c.TR, c.TW / 4, -c.TW / 4, -c.TW / 4, c.TW / 4, c.TW / 4;
+
+    Matrix<double, 2, 5> rlWheel = wheel;
+    Matrix<double, 2, 5> rrWheel = wheel;
+    Matrix<double, 2, 5> frWheel = wheel;
+    Matrix<double, 2, 5> flWheel = wheel;
+
+    if (steer > c.MAX_STEER) {
+        steer = c.MAX_STEER;
+    }
+
+    Matrix2d rot1;
+    Matrix2d rot2;
+    rot1 << cos(state[2]), -sin(state[2]), sin(state[2]), cos(state[2]);
+    rot2 << cos(steer), -sin(steer), sin(steer), cos(steer);
+
+    vehicle = rot1 * vehicle;
+    vehicle += Vector2d(state[0], state[1]).replicate(1, 5);
+    plot(vehicle.row(0), vehicle.row(1), color);
+
+    if (show_wheel) {
+        frWheel = rot2 * frWheel;
+        flWheel = rot2 * flWheel;
+
+        frWheel += Vector2d(c.WB, -c.WD / 2).replicate(1, 5);
+        flWheel += Vector2d(c.WB, c.WD / 2).replicate(1, 5);
+
+        rrWheel.row(1) -= VectorXd::Constant(5, c.WD / 2);
+        rlWheel.row(1) += VectorXd::Constant(5, c.WD / 2);
+
+        frWheel = rot1 * frWheel;
+        flWheel = rot1 * flWheel;
+        rrWheel = rot1 * rrWheel;
+        rlWheel = rot1 * rlWheel;
+
+        frWheel += Vector2d(state[0], state[1]).replicate(1, 5);
+        flWheel += Vector2d(state[0], state[1]).replicate(1, 5);
+        rrWheel += Vector2d(state[0], state[1]).replicate(1, 5);
+        rlWheel += Vector2d(state[0], state[1]).replicate(1, 5);
+
+        plot(frWheel.row(0), frWheel.row(1), color);
+        plot(flWheel.row(0), flWheel.row(1), color);
+        plot(rrWheel.row(0), rrWheel.row(1), color);
+        plot(rlWheel.row(0), rlWheel.row(1), color);
+    }
+    if (show_arrow) {
+        draw_arrow(state[0], state[1], state[2], c.WB * 0.8, color);
+    }
+}
+
+
 class Para {
 public:
     int minx;
@@ -476,8 +567,10 @@ int main(int argc, char** argv) {
     utils::VehicleConfig VC(4.5, 1.0, 3.0, 3.5, 0.5, 1.0, 0.6);
 
     plt::plot(obs[0], obs[1], "sk");
-    utils::draw_vehicle(start, 0.0, VC);
-    utils::draw_vehicle(goal, 0.0, VC, "0.4");
+    // utils::draw_vehicle(start, 0.0, VC);
+    // utils::draw_vehicle(goal, 0.0, VC, "0.4");
+    draw_vehicle1(start, 0.0, VC);
+    draw_vehicle1(goal, 0.0, VC, "0.4");
     plt::title("Hybrid A*");
     plt::pause(1.0);
 
@@ -503,9 +596,11 @@ int main(int argc, char** argv) {
             steer = 0.0;
         }
 
-        utils::draw_vehicle({path.x[idx], path.y[idx], path.yaw[idx]}, steer, VC);
+        // utils::draw_vehicle({path.x[idx], path.y[idx], path.yaw[idx]}, steer, VC);
+        draw_vehicle1({path.x[idx], path.y[idx], path.yaw[idx]}, steer, VC);
         if (idx < path.x.size() - 1) {
-            utils::draw_vehicle(goal, 0.0, VC, "0.4");
+            //utils::draw_vehicle(goal, 0.0, VC, "0.4");
+            draw_vehicle1(goal, 0.0, VC, "0.4");
         }
         plt::title("Hybrid A*");
         plt::axis("equal");
@@ -514,12 +609,15 @@ int main(int argc, char** argv) {
 
     plt::cla();
     plt::plot(obs[0], obs[1], "sk");
-    utils::draw_vehicle(start, 0.0, VC, "0.4");
-    utils::draw_vehicle(goal, 0.0, VC);
+    // utils::draw_vehicle(start, 0.0, VC, "0.4");
+    // utils::draw_vehicle(goal, 0.0, VC);
+    draw_vehicle1(start, 0.0, VC, "0.4");
+    draw_vehicle1(goal, 0.0, VC);
     plt::title("Hybrid A*");
     plt::axis("equal");
     for (size_t idx = 2; idx < path.x.size(); idx += 4) {
-        utils::draw_vehicle({path.x[idx], path.y[idx], path.yaw[idx]}, 0, VC, "-c", false, false);
+        //utils::draw_vehicle({path.x[idx], path.y[idx], path.yaw[idx]}, 0, VC, "-c", false, false);
+        draw_vehicle1({path.x[idx], path.y[idx], path.yaw[idx]}, 0, VC, "-c", false, false);
         plt::pause(0.001);
     }
 
