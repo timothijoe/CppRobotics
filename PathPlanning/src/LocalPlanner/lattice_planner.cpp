f2f0ee08e6eff0ed186ba8c99b676eae2a119ad8
timothijoe <zt1301112@gmail.com>
2024-04-18 17:54:18 +0800

    add draw vehicle into lattice planner to avoid undefined symbol problem

--- PathPlanning/src/LocalPlanner/lattice_planner.cpp
+++ PathPlanning/src/LocalPlanner/lattice_planner.cpp
@@ -15,6 +15,7 @@
 
 using std::vector;
 using namespace Eigen;
+//using namespace utils;
 namespace plt = matplotlibcpp;
 
 constexpr double ROAD_WIDTH = 8.0;
@@ -32,6 +33,97 @@ constexpr double K_COLLISION = 500;
 constexpr double MAX_SPEED = 50.0 / 3.6;
 constexpr double MAX_ACCEL = 8.0;
 constexpr double MAX_CURVATURE = 6.0;
+
+static void plot(VectorXd vec_x, VectorXd vec_y, std::string style) {
+    std::vector<double> x;
+    std::vector<double> y;
+    double* x_pointer = vec_x.data();
+    double* y_pointer = vec_y.data();
+    x.assign(x_pointer, x_pointer + vec_x.rows());
+    y.assign(y_pointer, y_pointer + vec_y.rows());
+
+    plt::plot(x, y, style);
+}
+
+void draw_arrow(double x, double y, double theta, double L, std::string color) {
+    double angle = M_PI / 6;
+    double d = 0.3 * L;
+
+    double x_start = x;
+    double y_start = y;
+    double x_end = x + L * cos(theta);
+    double y_end = y + L * sin(theta);
+
+    double theta_hat_L = theta + M_PI - angle;
+    double theta_hat_R = theta + M_PI + angle;
+
+    double x_hat_start = x_end;
+    double x_hat_end_L = x_hat_start + d * cos(theta_hat_L);
+    double x_hat_end_R = x_hat_start + d * cos(theta_hat_R);
+
+    double y_hat_start = y_end;
+    double y_hat_end_L = y_hat_start + d * sin(theta_hat_L);
+    double y_hat_end_R = y_hat_start + d * sin(theta_hat_R);
+
+    plt::plot({x_start, x_end}, {y_start, y_end}, color);
+    plt::plot({x_hat_start, x_hat_end_L}, {y_hat_start, y_hat_end_L}, color);
+    plt::plot({x_hat_start, x_hat_end_R}, {y_hat_start, y_hat_end_R}, color);
+}
+
+void draw_vehicle1(Vector3d state, double steer, utils::VehicleConfig c, std::string color= "-k", bool show_wheel = true,
+                  bool show_arrow= true) {
+    Matrix<double, 2, 5> vehicle;
+    Matrix<double, 2, 5> wheel;
+    vehicle << -c.RB, -c.RB, c.RF, c.RF, -c.RB, c.W / 2, -c.W / 2, -c.W / 2, c.W / 2, c.W / 2;
+    wheel << -c.TR, -c.TR, c.TR, c.TR, -c.TR, c.TW / 4, -c.TW / 4, -c.TW / 4, c.TW / 4, c.TW / 4;
+
+    Matrix<double, 2, 5> rlWheel = wheel;
+    Matrix<double, 2, 5> rrWheel = wheel;
+    Matrix<double, 2, 5> frWheel = wheel;
+    Matrix<double, 2, 5> flWheel = wheel;
+
+    if (steer > c.MAX_STEER) {
+        steer = c.MAX_STEER;
+    }
+
+    Matrix2d rot1;
+    Matrix2d rot2;
+    rot1 << cos(state[2]), -sin(state[2]), sin(state[2]), cos(state[2]);
+    rot2 << cos(steer), -sin(steer), sin(steer), cos(steer);
+
+    vehicle = rot1 * vehicle;
+    vehicle += Vector2d(state[0], state[1]).replicate(1, 5);
+    plot(vehicle.row(0), vehicle.row(1), color);
+
+    if (show_wheel) {
+        frWheel = rot2 * frWheel;
+        flWheel = rot2 * flWheel;
+
+        frWheel += Vector2d(c.WB, -c.WD / 2).replicate(1, 5);
+        flWheel += Vector2d(c.WB, c.WD / 2).replicate(1, 5);
+
+        rrWheel.row(1) -= VectorXd::Constant(5, c.WD / 2);
+        rlWheel.row(1) += VectorXd::Constant(5, c.WD / 2);
+
+        frWheel = rot1 * frWheel;
+        flWheel = rot1 * flWheel;
+        rrWheel = rot1 * rrWheel;
+        rlWheel = rot1 * rlWheel;
+
+        frWheel += Vector2d(state[0], state[1]).replicate(1, 5);
+        flWheel += Vector2d(state[0], state[1]).replicate(1, 5);
+        rrWheel += Vector2d(state[0], state[1]).replicate(1, 5);
+        rlWheel += Vector2d(state[0], state[1]).replicate(1, 5);
+
+        plot(frWheel.row(0), frWheel.row(1), color);
+        plot(flWheel.row(0), flWheel.row(1), color);
+        plot(rrWheel.row(0), rrWheel.row(1), color);
+        plot(rlWheel.row(0), rlWheel.row(1), color);
+    }
+    if (show_arrow) {
+        draw_arrow(state[0], state[1], state[2], c.WB * 0.8, color);
+    }
+}
 
 class Path {
 public:
@@ -361,7 +453,8 @@ void cruise_case(const utils::VehicleConfig& vc) {
         plt::plot(outxy[0], outxy[1], {{"linewidth", "2"}, {"color", "k"}});
         plt::named_plot("Optimal trajectory", path.x, path.y, "-r");
         plt::plot(obs[0], obs[1], "ok");
-        utils::draw_vehicle({path.x[1], path.y[1], path.yaw[1]}, steer, vc);
+        //utils::draw_vehicle({path.x[1], path.y[1], path.yaw[1]}, steer, vc);
+        draw_vehicle1({path.x[1], path.y[1], path.yaw[1]}, steer, vc);
         plt::title("Lattice Planner in Cruising Scene V[km/h]:" +
                    std::to_string(s0_v * 3.6).substr(0, 4));
         plt::axis("equal");
@@ -413,7 +506,7 @@ void stop_case(const utils::VehicleConfig& vc) {
         plt::plot(upxy[0], upxy[1], {{"linewidth", "2"}, {"color", "k"}});
         plt::plot(downxy[0], downxy[1], {{"linewidth", "2"}, {"color", "k"}});
         plt::named_plot("Optimal trajectory", path.x, path.y, "-r");
-        utils::draw_vehicle({path.x[1], path.y[1], path.yaw[1]}, steer, vc);
+        //utils::draw_vehicle({path.x[1], path.y[1], path.yaw[1]}, steer, vc);
         plt::title("Lattice Planner in Stopping Scene V[km/h]:" +
                    std::to_string(s0_v * 3.6).substr(0, 4));
         plt::axis("equal");
